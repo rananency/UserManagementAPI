1d004c68ff39504b198df888d2e3062e05dd422c
Champakbhai <rana-nency.champakbhai@capgemini.com>
2025-01-30 14:44:18 +0530

    Setup MongoDB Connection

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const connectDB = require('./config/database');
 
 const app = express();
+
+connectDB();
 
 app.listen(3000,()=>{
     console.log("server started")
